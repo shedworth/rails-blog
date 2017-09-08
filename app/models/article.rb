f640be0ff2a54531973a99cfa23a78c64bdacc96
shedworth <tim.steer@hotmail.co.uk>
2017-09-08 16:50:16 +0100

    Added author and default values

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,34 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+  before_validation :nillify_parameters,
+                    :set_default_author
+                    
+  scope :troll, -> { where(author: "J. Trollington") } # method can be used in database query
+  
+  def default_author
+    if author 
+      author 
+    else
+      'joe'
+    end  
+  end
+  
+  private
+    def nillify_parameters
+    
+      arr = ["text", "author"]
+      arr.each do |x|
+        if send(x) == ""
+          send("#{x}=", nil)
+        end
+      end
+    end
+    
+    def set_default_author
+      if author == "test"
+        self.author = "J. Bloggs"
+      end
+    end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905123345) do
+ActiveRecord::Schema.define(version: 20170908121204) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
